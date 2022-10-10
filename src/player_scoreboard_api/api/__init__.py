b2331b5967d7854227f6dbecf69ad9f073596616
Nakou <alain.krzeczowski@hotmail.fr>
2022-10-10 17:40:33 +0200

    #1 Add player in container.py and api/__init__.py

--- src/player_scoreboard_api/api/__init__.py
+++ src/player_scoreboard_api/api/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 
 from .health import health_router
+from .player import player_router
 
 
 API_ROUTER_PREFIX = "/api"
@@ -10,3 +11,4 @@ api_router = APIRouter(
     prefix=API_ROUTER_PREFIX
 )
 api_router.include_router(health_router)
+api_router.include_router(player_router)
--- src/player_scoreboard_api/container.py
+++ src/player_scoreboard_api/container.py
@@ -3,6 +3,7 @@ from dependency_injector import containers, providers
 
 
 from .service.health import HealthService
+from .service.player_service import PlayerService
 
 class Container(containers.DeclarativeContainer):
 
@@ -15,3 +16,4 @@ class Container(containers.DeclarativeContainer):
     application = providers.Dependency()
 
     health_service: HealthService = providers.Singleton(HealthService)
+    player_service: PlayerService = providers.Singleton(PlayerService)
